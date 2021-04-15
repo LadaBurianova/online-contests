9b6db52b0c1371e06278a45a5c9718024fe2d439
LadaBurianova <bulanova.dasha2004@yandex.ru>
2021-04-15 23:34:48 +0300

    register/login/logout (finished)

--- db_interaction/teams.py
+++ db_interaction/teams.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from .__db_session import SqlAlchemyBase
 import sqlalchemy.orm as orm
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -11,7 +12,7 @@ class Team(SqlAlchemyBase):
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
@@ -22,7 +23,8 @@ class User(SqlAlchemyBase):
     team = orm.relation('Team')
 
     def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+        print(self.password, password)
+        return check_password_hash(self.password, password)
