9cd2b394956bc1d375e5965cf35b7e3f150a7c22
LadaBurianova <bulanova.dasha2004@yandex.ru>
2021-04-20 18:59:41 +0300

    add_to_db without import error add_to db without error

--- db_interaction/problems.py
+++ db_interaction/problems.py
@@ -5,8 +5,8 @@ from .__db_session import SqlAlchemyBase
 class Problem(SqlAlchemyBase):
     __tablename__ = 'problems'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    number = sqlalchemy.Column(sqlalchemy.Integer, unique=True)  # number - номер строчки. Баллы - то же число.
-    category = sqlalchemy.Column(sqlalchemy.String, unique=True)  # category - название типа задач (колонки)
+    number = sqlalchemy.Column(sqlalchemy.Integer)  # number - номер строчки. Баллы - то же число.
+    category = sqlalchemy.Column(sqlalchemy.String)  # category - название типа задач (колонки)
     problem_text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     picture_link = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     correct_answer = sqlalchemy.Column(sqlalchemy.String)
